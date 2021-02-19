9738ef500131db1526335f97de72bb7c28c72e0f
ts6075 <75653667+ts6075@users.noreply.github.com>
2021-02-19 19:34:08 +0800

    feat: 基本場景設定

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D playerRigidbody2D;
+
+    /// <summary>
+    /// 水平推力
+    /// </summary>
+    public float forceX;
+
+    /// <summary>
+    /// 垂直推力
+    /// </summary>
+    public float forceY;
+
+    void Start()
+    {
+        playerRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            playerRigidbody2D.AddForce(new Vector2(forceX * -1, 0));
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            playerRigidbody2D.AddForce(new Vector2(forceX, 0));
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRigidbody2D.AddForce(new Vector2(0, forceY));
+        }
+    }
+}
