fc96c29208f964a3101ae04778692b29a3c4b7ab
ts6075 <75653667+ts6075@users.noreply.github.com>
2021-02-20 01:38:14 +0800

    feat: 調校角色控制、降低起始平台位置、增加角色動畫、修正材質

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,7 +11,7 @@ public class PlayerController : MonoBehaviour
     /// <summary>
     /// 最大水平速度
     /// </summary>
-    private float maxVelocityX = 10;
+    private float maxVelocityX = 3.0f;
     /// <summary>
     /// 水平推力
     /// </summary>
@@ -35,22 +35,33 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            if (Mathf.Abs(playerRigidbody2D.velocity.x) < maxVelocityX)
-            {
-                playerRigidbody2D.AddForce(new Vector2(forceX * -1, 0));
-            }
+            playerRigidbody2D.velocity = new Vector2(maxVelocityX * -1, playerRigidbody2D.velocity.y);
+            //if (Mathf.Abs(playerRigidbody2D.velocity.x) < maxVelocityX)
+            //{
+            //    playerRigidbody2D.AddForce(new Vector2(forceX * -1, 0));
+            //}
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
-            if (Mathf.Abs(playerRigidbody2D.velocity.x) < maxVelocityX)
-            {
-                playerRigidbody2D.AddForce(new Vector2(forceX, 0));
-            }
+            playerRigidbody2D.velocity = new Vector2(maxVelocityX, playerRigidbody2D.velocity.y);
+            //if (Mathf.Abs(playerRigidbody2D.velocity.x) < maxVelocityX)
+            //{
+            //    playerRigidbody2D.AddForce(new Vector2(forceX, 0));
+            //}
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             playerRigidbody2D.AddForce(new Vector2(0, forceY));
+        }
+
+        if (playerRigidbody2D.velocity.x < 0)
+        {
+            gameObject.transform.rotation = new Quaternion(0, 180, 0, 0);
+        }
+        else
+        {
+            gameObject.transform.rotation = new Quaternion(0, 0, 0, 0);
         }
     }
 }
