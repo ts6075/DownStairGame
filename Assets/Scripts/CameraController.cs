1bca92594a326e5f0f7ba8f91e0a75153b3f6158
ts6075 <75653667+ts6075@users.noreply.github.com>
2021-02-19 19:36:28 +0800

    feat: 攝影機移動

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    /// <summary>
+    /// 下降速度
+    /// </summary>
+    public float downSpeed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(0, -downSpeed * Time.deltaTime, 0);
+    }
+}
