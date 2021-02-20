3f2e1a29f01cde3d04067487f7f3da84c64fed0b
ts6075 <75653667+ts6075@users.noreply.github.com>
2021-02-20 23:20:56 +0800

    feat: 加入果凍平台

---
+++ Assets/Scripts/JellyController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class JellyController : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 5);
+        }
+    }
+}
